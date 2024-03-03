1edbb114a030a9c8bacc75ad080cd8077b03bdcc
65010902_Racha_Tungtrakool <65010902@kmitl.ac.th>
2024-03-03 11:40:30 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBDEV_Pantip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
